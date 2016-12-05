cbc4a84af94477117ee35304e798625bb0499966
渡邉　知樹 <kazuki7932@gmail.com>
2016-12-06 02:26:41 +0900

    fix migration bug

--- db/migrate/20161205122746_create_customers_passwords.rb
+++ db/migrate/20161205122746_create_customers_passwords.rb
@@ -2,11 +2,12 @@ class CreateCustomersPasswords < ActiveRecord::Migration[5.0]
   def change
     create_table :customers_passwords do |t|
       t.integer :customer_id, null: false
-      t.foreign_key :customers, dependent: :delete, name: "customers_passwords_customer_id"
       t.integer :password_id, null: false
-      t.foreign_key :passwords, dependent: :delete, name: "customers_passwords_password_id"
       t.timestamps
     end
+
+    execute "ALTER TABLE `customers_passwords` ADD FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE;"
+    execute "ALTER TABLE `customers_passwords` ADD FOREIGN KEY (`password_id`) REFERENCES `passwords` (`id`) ON DELETE CASCADE;"
 
 
   end
