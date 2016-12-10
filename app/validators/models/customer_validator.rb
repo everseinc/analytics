861346cc7585f4e3b0917d56d9ca4c13d3c072b9
渡邉　知樹 <kazuki7932@gmail.com>
2016-12-11 03:28:39 +0900

    fix git error

---
+++ app/validators/models/customer_validator.rb
@@ -0,0 +1,8 @@
+class Models::CustomerValidator < ActiveModel::Validator
+  def validate(customer)
+    condition = Maybe.new(customer.name) >> StringValidator.not_empty >> StringValidator.not_only_blank
+    customer.errors[:name] << 'name is invalid'
+  end
+
+
+end
