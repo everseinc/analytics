aaf420319e735abcc7becfe0aae59bb98dceba36
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-12-11 17:30:02 +0900

    remove test codes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,5 @@
 class CustomersController < ApplicationController
   def new
-  	f = -> (value, monad) { monad.right value + 10 }
-  	g = -> (value, monad) { monad.right value * 2 }
-  	h = -> (value, monad) { monad.left Minor::ArgumentError.code(10000) }
-  	@test = Either.right(100) >> f >> h >> g
     @form = CustomerForm.new
   end
 end
