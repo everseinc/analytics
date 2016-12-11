a08469f1f85d2e8d77f699514c7b3d58c9e72acb
難波 啓司 <keiji_1110@yahoo.co.jp>
2016-12-11 17:50:57 +0900

    change validation from Maybe to Either

--- app/validators/number_validator.rb
+++ app/validators/number_validator.rb
@@ -11,48 +11,48 @@ class NumberValidator < BaseValidator
 	# a number must not be zero
 	def self.not_zero
 		condition = -> (number) { number != 0 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be above zero
 	def self.above_zero
 		condition = -> (number) { number > 0 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be above target
 	def self.above(target)
 		condition = -> (number) { number > target }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be under zero
 	def self.under_zero
 		condition = -> (number) { number < 0 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be under target
 	def self.under(target)
 		condition = -> (number) { number < target }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be even number
 	def self.even
 		condition = -> (number) { number % 2 == 1 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be odd number
 	def self.odd
 		condition = -> (number) { number % 2 == 0 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 
 	# a number must be divisible by target
 	def self.divisible_by(target)
 		condition = -> (number) { number % target == 0 }
-		validate(&condition)
+		either_validate(&condition, 21001)
 	end
 end
