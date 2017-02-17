73f253386549cfb5c58a3fddec4779990a518f34
kazuuuuuki <kazuki7932@gmail.com>
2017-02-17 09:25:46 +0100

    fix bug about customers_passwords

--- app/factories/customer_details.rb
+++ app/factories/customer_details.rb
@@ -1,14 +1,18 @@
 class CustomerDetails
+  extend DigestManager
   attr_accessor :name, :email, :password, :password_confirmation
   class << self
     def save(param)
       customer = Customer.new(name: param[:name], email: param[:email])
       password = Password.new(password: param[:password], password_confirmation: param[:password_confirmation])
 
-      customer.save!
-      password.save!
+      if customer.save && password.save
+        CustomersPassword.create(customer_id: customer.id, password_id: password.id)
+      else
+        raise Major::SaveFailedError.code(21007)
+      end
 
-      customer = Customer.find_by(email: param[:email])
+      customer
     end
   end
 
