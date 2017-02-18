b68920f7bb0cdf5b045ca385e2b68c1b0b59fa23
kazuuuuuki <kazuki7932@gmail.com>
2017-02-18 12:11:40 +0100

    create report header

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < MainLayoutController
-  def main
+  def test
+    @project = {:project_name => "Test"}
+    @emo_blocks = [{started_at: "2017年2月10日15時00分", ended_at: "2017年2月10日16時00分"}, {started_at: "2017年2月10日16時00分", ended_at: "2017年2月10日17時00分"}]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root :to => 'customers#new'
 
-  get "reports/test", to: "reports#main"
+  get "reports/test", to: "reports#test"
 
 
   resources :customers, :only => [:new, :create, :show]
