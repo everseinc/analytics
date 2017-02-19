26c7cf5ab98e430d2f046f181b41c064891619fa
Yuki Yamashita <yyama0704@gmail.com>
2017-02-19 14:34:08 +0900

    create reports sub navigation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < MainLayoutController
   def test
+  	@projects = [{:project_name => "Test"}, {:project_name => "Test1"}, {:project_name => "Test2"}]
     @project = {:project_name => "Test"}
     @emo_blocks = [{started_at: "2017年2月10日15時00分", ended_at: "2017年2月10日16時00分"}, {started_at: "2017年2月10日16時00分", ended_at: "2017年2月10日17時00分"}]
   end
