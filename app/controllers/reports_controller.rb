059199730b030a98a536c28cf985e5f67e98e632
kazuuuuuki <kazuki7932@gmail.com>
2017-02-20 12:53:45 +0100

    initialize chart.js

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < MainLayoutController
-  def test
+  def main
   	@projects = [{:project_name => "Test"}, {:project_name => "Test1"}, {:project_name => "Test2"}]
     @project = {:project_name => "Test"}
     @emo_blocks = [{started_at: "2017年2月10日15時00分", ended_at: "2017年2月10日16時00分"}, {started_at: "2017年2月10日16時00分", ended_at: "2017年2月10日17時00分"}]
