613c57e1d4dfaa1d18a16fd38af95a0b28c55383
kazuuuuuki <kazuki7932@gmail.com>
2017-02-24 14:28:09 +0100

    change routes and set get_emo func

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
 class ReportsController < MainLayoutController
-  def test
+  def show
+  # need to check customer anthentificate
+  EmoDetail.get_emo_by(:project_id, :arg_start_time, :arg_end_time)
   	@projects = [{:project_name => "Test"}, {:project_name => "Test1"}, {:project_name => "Test2"}]
     @project = {:project_name => "Test"}
     @emo_blocks = [{started_at: "2017年2月10日15時00分", ended_at: "2017年2月10日16時00分"}, {started_at: "2017年2月10日16時00分", ended_at: "2017年2月10日17時00分"}]
