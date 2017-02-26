8382bd5f7bed9e2abe3d178c2f7ef314e6ad3735
keijinamba <keiji_1110@yahoo.co.jp>
2017-02-27 03:50:59 +0900

    enable to update customer name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,5 +17,26 @@ class Customer < ApplicationRecord
   has_many :apps_customers, dependent: :destroy
   has_many :apps, through: :apps_customers
 
+
+  ###
+  ## class methods
+  #
+
+  class << self
+
+
+    ###
+    ## update
+    #
+
+    def update_name(customer_params)
+      customer = self.find(customer_params[:id])
+      if customer.update(name: customer_params[:name])
+        return customer
+      else
+        raise Major::UpdateFailedError.code(21008)
+      end
+    end
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 
 
-  resources :customers, :only => [:new, :create, :show]
+  resources :customers, :only => [:new, :create, :edit, :update, :show]
   resources :apps, :only => [:new, :create, :index, :show]
   resources :projects, :only => [:new, :create, :index, :show]
 
