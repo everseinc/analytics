e21e337959bee5fb633bfe4c4486f2ee20e4d6fb
keijinamba <keiji_1110@yahoo.co.jp>
2017-02-28 03:33:57 +0900

    change app index page to collections view

---
+++ app/assets/javascripts/common/1_1_header_customer_menu.js
@@ -0,0 +1,3 @@
+var element = document.getElementById('header-customer-menu')
+var headerCustomerMenu = new pullDownMenu(element);
+
--- app/assets/javascripts/common/2_1_header.js
+++ app/assets/javascripts/common/2_1_header.js
@@ -1,1 +1,2 @@
 headerMenuButton.onClick();
+headerCustomerMenuButton.onClick();
