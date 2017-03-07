74331567ccd5bbaf77a1aff36ee66ecfbac5ada5
keijinamba <keiji_1110@yahoo.co.jp>
2017-03-07 10:29:08 +0900

    change some codes

--- app/assets/javascripts/reports/1_1_chart_setter.js
+++ app/assets/javascripts/reports/1_1_chart_setter.js
@@ -2,6 +2,7 @@ var chartSetter = {
 	chart_renderer: null,
 	new: function(report) {
 		this.chart_renderer = new chartRenderer(report);
+		this.reload();
 	},
 	reload: function() {
 		this.chart_renderer.reload();
