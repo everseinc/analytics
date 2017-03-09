0286234bdc510ab7ff9d248b3852d4cee57bfcda
keijinamba <keiji_1110@yahoo.co.jp>
2017-03-09 15:19:22 +0900

    remove console.log

--- app/assets/javascripts/reports/2_1_report.js
+++ app/assets/javascripts/reports/2_1_report.js
@@ -4,10 +4,6 @@ report.setReport();
 
 (function() {
 
-	var dim_stores = new DimStores(report.dim_stores);
-	console.log(dim_stores.dim_stores[0].allEmoBlocks(report.emo_details));
-
-
 	/* set default chart */
 	chartSetter.new(report);
 
