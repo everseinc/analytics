d3e9dbfbc5bb8f94ae13085ee5932dd307f55fc5
keijinamba <keiji_1110@yahoo.co.jp>
2017-04-04 15:27:51 +0900

    fix some bugs

--- app/exceptions/major/records/delete_failed_error.rb
+++ app/exceptions/major/records/delete_failed_error.rb
@@ -2,7 +2,7 @@
 ## if some models fail to delete records, this error will be thrown
 ##
 
-class MajorError::Validatoins::DeleteFailedError < CustomError
+class Major::Records::DeleteFailedError < Major::BaseError
 	
 	###
 	## public methods
--- app/validators/number_validator.rb
+++ app/validators/number_validator.rb
@@ -11,48 +11,48 @@ class NumberValidator < BaseValidator
 	# a number must not be zero
 	def self.not_zero
 		condition = -> (number) { number != 0 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be above zero
 	def self.above_zero
 		condition = -> (number) { number > 0 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be above target
 	def self.above(target)
 		condition = -> (number) { number > target }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be under zero
 	def self.under_zero
 		condition = -> (number) { number < 0 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be under target
 	def self.under(target)
 		condition = -> (number) { number < target }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be even number
 	def self.even
 		condition = -> (number) { number % 2 == 1 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be odd number
 	def self.odd
 		condition = -> (number) { number % 2 == 0 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 
 	# a number must be divisible by target
 	def self.divisible_by(target)
 		condition = -> (number) { number % target == 0 }
-		either_validate(&condition, 21001)
+		either_validate(21001, &condition)
 	end
 end
