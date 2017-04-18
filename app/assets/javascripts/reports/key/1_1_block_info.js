058fb7c5c3989a68a00eb9ed1b18fe62fb3d389d
keijinamba <keiji_1110@yahoo.co.jp>
2017-04-18 23:45:38 +0900

    change block info area

--- app/assets/javascripts/reports/key/1_1_block_info.js
+++ app/assets/javascripts/reports/key/1_1_block_info.js
@@ -1,20 +1,21 @@
 var BlockInfo = {
 	element: document.getElementById("block_info_container"),
+	tbody: document.getElementById("block_info_container_tbody"),
 	children: [],
 	append: function(block) {
-		var div = document.createElement('div');
-		div.classList.add('info_block');
-		div.classList.add('layer-cell');
+		var div = document.createElement('tr');
+		// div.classList.add('info_block');
+		// div.classList.add('layer-cell');
 		div.innerHTML = this.templete(block);
 
-		// this.element.appendChild(div);
+		this.tbody.appendChild(div);
 		this.children.push({ id: block.id, dom: div});
 		this.sort();
 	},
 	remove: function(block) {
 		var dom = this.children.filter(function(v) { return v.id == block.id }).first().dom;
 
-		this.element.removeChild(dom);
+		this.tbody.removeChild(dom);
 		this.children.some(function(v, i){
 		    if (v.id == block.id) this.children.splice(i, 1);    
 		}.bind(this));
@@ -22,29 +23,16 @@ var BlockInfo = {
 	sort: function() {
     this.children
     	.sort(function(a, b) { return a.id > b.id })
-    	.forEach(function(v) { return this.element.appendChild(v.dom) }.bind(this));
+    	.forEach(function(v) { return this.tbody.appendChild(v.dom) }.bind(this));
 	},
 
 	templete: function(block) {
 		return '\
-			<table>\
-				<tr>\
-					<td>key</td>\
-					<td>' + block.key + '</td>\
-				</tr>\
-				<tr>\
-					<td>開始時刻</td>\
-					<td>' + block.started_at.default() + '</td>\
-				</tr>\
-				<tr>\
-					<td>終了時刻</td>\
-					<td>' + block.ended_at.default() + '</td>\
-				</tr>\
-				<tr>\
-					<td>レコード数</td>\
-					<td>' + block.records.length + '</td>\
-				</tr>\
-			</table>\
+			<td>' + block.key + '</td>\
+			<td>' + block.started_at.default() + '</td>\
+			<td>' + block.ended_at.default() + '</td>\
+			<td>' + block.records.length + '</td>\
+			<td><div style="background-color: ' + block.color() + '; width: 12px; height: 12px;"></div></td>\
 		';
 	},
 }
